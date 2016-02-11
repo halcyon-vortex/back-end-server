410e0cbf2ce02d316d67593c892bccf5cb96e790
Eochs <slochastic@gmail.com>
2016-02-10 20:20:51 -0800

    changed to send json to front end

--- server/api/trending/trendingController.js
+++ server/api/trending/trendingController.js
@@ -14,13 +14,17 @@ exports.getTrending = function(req, res, next) {
   var key = language+":"+time_range+"_"+trending_modifier;
 
   client.zrevrange(key, 0, -1, 'withscores', function (err, members){
-    if (err) throw(err)
-    console.log(members);
-    var lists=_.groupBy(members, function(a,b) {
-        return Math.floor(b/2);
-    });
-    console.log( _.toArray(lists) );
-    res.send(_.toArray(lists))
+    if (err) throw(err);
+    // console.log('all the members: ', members, '\n');
+
+    json_members = [];
+    for (var i = 0; i < members.length-1; i+=2) {
+      var repo_info = JSON.parse(members[i]);
+      repo_info['score'] = Number(members[i+1]);
+      json_members.push(repo_info);
+    }
+    // console.log('json members: ', json_members );
+    res.json( json_members );
   });
 
 };
